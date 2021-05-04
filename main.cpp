f15ffb53032401b1943d4220e8dd2306ef5db920
Sergey Prika <prikasp@yandex-team.ru>
2021-05-04 09:31:31 +0300

    Merge files

--- main.cpp
+++ main.cpp
@@ -12,14 +12,14 @@
 namespace {
 const int kDepShift = 100;
 const int kThreads = 8;
-const int kDumpEvery = 50000;
+const int kDumpEvery = 31250;
 const int kSaveThreshold = 20;
 } // namespace
 
 using namespace date;
 
 using IdT = unsigned int;
-using SparseMatrix = std::unordered_map<IdT, std::unordered_map<IdT, double>>;
+using SparseMatrix = std::unordered_map<IdT, std::unordered_map<IdT, int>>;
 
 struct Data {
   SparseMatrix deps;
@@ -63,8 +63,9 @@ void Save(Data &&data, const std::string &filename) {
         }
       }
     }
-    os << it->first << kSep << it->second.size() << kSep
-       << data.popularity.at(it->first) << std::endl;
+    // Suppose, popularity is useless
+    os << it->first << kSep << it->second.size() << kSep << /*popularity=*/0
+       << std::endl;
     for (const auto jt : it->second) {
       os << jt.first << kSep << jt.second << std::endl;
     }
@@ -84,7 +85,8 @@ Data Load(const std::string &filename) {
   for (int i = 0; i < tracks_cnt; i++) {
     int id, deps_cnt, popularity;
     is >> id >> deps_cnt >> popularity;
-    res.popularity[id] = popularity;
+    // Suppose, popularity is useless
+    // res.popularity[id] = popularity;
     auto &track_deps = res.deps[id];
     for (int j = 0; j < deps_cnt; j++) {
       int dep_tr_id, weight;
@@ -148,7 +150,6 @@ void ConstructAndSaveData(std::vector<User> &&users, int batch_id) {
 }
 
 int ReadAndSave() {
-  std::cout << "started_at " << std::chrono::system_clock::now() << std::endl;
   std::ifstream is("data_train_5kk.yson");
   std::string line;
   std::vector<User> users;
@@ -175,8 +176,42 @@ int ReadAndSave() {
   for (auto &fut : tasks) {
     fut.get();
   }
-  std::cout << "finished at " << std::chrono::system_clock::now() << std::endl;
   return 0;
 }
 
-int main() { return ReadAndSave(); }
+void Merge(Data &data, const std::string &filename) {
+  auto &deps = data.deps;
+  auto new_data = Load(filename);
+  for (auto it = new_data.deps.begin(); it != new_data.deps.end(); it++) {
+    auto &tr_deps = deps[it->first];
+    for (auto jt = it->second.begin(); jt != it->second.end(); jt++) {
+      auto old_it = tr_deps.find(jt->first);
+      // first, no new threshold
+      if (old_it != tr_deps.end()) {
+        old_it->second += jt->second;
+      } else {
+        tr_deps.insert(*jt);
+      }
+    }
+  }
+}
+
+void MergeAndSave() {
+  Data res;
+  for (auto batch_id = 0; batch_id < kThreads; batch_id++) {
+    std::cout << "Start merge batch_id " << batch_id << " at "
+              << std::chrono::system_clock::now();
+    for (auto dump_id = 0; dump_id < 20; dump_id++) {
+      auto filename =
+          "r_data_" + std::to_string(batch_id) + "_" + std::to_string(dump_id);
+      Merge(res, filename);
+    }
+  }
+}
+
+int main() {
+  std::cout << "started_at " << std::chrono::system_clock::now() << std::endl;
+  ReadAndSave();
+  std::cout << "finished at " << std::chrono::system_clock::now() << std::endl;
+  return 0;
+}
